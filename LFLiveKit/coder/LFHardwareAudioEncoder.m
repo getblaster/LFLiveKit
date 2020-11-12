109670c6c433d9272797b794e393056a255c869d
Nikolay Borisov <casper6479@gmail.com>
2020-11-12 03:22:49 +0300

    fix wrong memset

--- LFLiveKit/coder/LFHardwareAudioEncoder.m
+++ LFLiveKit/coder/LFHardwareAudioEncoder.m
@@ -67,7 +67,7 @@
         char *totalBuf = malloc(totalSize);
         char *p = totalBuf;
         
-        memset(totalBuf, (int)totalSize, 0);
+        memset(totalBuf, 0, (int)totalSize);
         memcpy(totalBuf, leftBuf, leftLength);
         memcpy(totalBuf + leftLength, audioData.bytes, audioData.length);
         
