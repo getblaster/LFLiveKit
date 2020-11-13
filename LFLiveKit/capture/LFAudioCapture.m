4a8bb3137298be7b007982e3d22b19b1b023a917
Nikolay Borisov <casper6479@gmail.com>
2020-11-13 20:58:07 +0300

    Update LFAudioCapture.m

--- LFLiveKit/capture/LFAudioCapture.m
+++ LFLiveKit/capture/LFAudioCapture.m
@@ -115,6 +115,7 @@ NSString *const LFAudioComponentFailedToCreateNotification = @"LFAudioComponentF
         dispatch_async(self.taskQueue, ^{
             self.isRunning = YES;
             NSLog(@"MicrophoneSource: startRunning");
+            [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker | AVAudioSessionCategoryOptionInterruptSpokenAudioAndMixWithOthers error:nil];
             AudioOutputUnitStart(self.componetInstance);
         });
     } else {
